ea8657891c73e37289fd93f8d40d6524285ec059
Mauro <maurolopes.dev@gmail.com>
2024-05-07 10:33:25 -0300

    feat: search filter on home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "../axios-config";
 import { Link } from "react-router-dom";
-
 import "./Home.css";
 
 const Home = () => {
   const [memories, setMemories] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const getMemories = async () => {
@@ -15,12 +15,26 @@ const Home = () => {
     getMemories();
   }, []);
 
+  const searchLowerCase = search.toLocaleLowerCase();
+  const filterMemories = memories.filter(
+    (memory) =>
+      memory.title.toLowerCase().includes(searchLowerCase) ||
+      memory.description.toLowerCase().includes(searchLowerCase)
+  );
+
   return (
     <div className="home">
+      <input
+        type="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="search-input"
+        placeholder="Pesquise suas memórias "
+      />
       <h2>Confira as suas memórias</h2>
       <div className="memories-container">
         {memories.length > 0 &&
-          memories.map((memory) => (
+          filterMemories.map((memory) => (
             <div className="memory" key={memory._id}>
               <Link to={`/memories/${memory._id}`}>
                 <img
